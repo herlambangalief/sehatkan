0c4ec00a36378d4dc9f43a2fbcdd36bb3d7e822b
User <m.herlambangalif@gmail.com>
2024-04-27 20:01:28 +0700

    update index.php

--- html_menu_1/component/header.php
+++ html_menu_1/component/header.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['username'])) {
 }
 ?>
 
-<header class="header_sticky">
+	<header class="header_sticky">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-3 col-6">
@@ -14,7 +14,7 @@ if (isset($_SESSION['username'])) {
 						<a href="index.php" title="Findoctor"><img src="img/logo1.png" data-retina="true" alt="" width="163" height="36"></a>
 					</div>
 				</div>
-				<nav class="col-md-9 ">
+				<nav class="col-md-9 d-lg-block d-none"> <!-- Menambahkan kelas untuk menampilkan navigasi hanya pada layar besar -->
 					<ul id="top_access">
 						<li id="user">
 							<?php 
@@ -34,8 +34,7 @@ if (isset($_SESSION['username'])) {
 								<ul>
 									<li><a href="artikel.php">Artikel</a></li>
 									<li><a href="penyakit.php">Penyakit</a></li>
-									<li><a href="obat.php">Obat</a></li>
-									
+									<li><a href="obat.php">Obat</a></li>	
 								</ul>
 							</li>
 							<li><a href="dokter.php"><i class="icon-user-md"></i>Dokter</a></li>
@@ -55,6 +54,57 @@ if (isset($_SESSION['username'])) {
 					</div>
 					<!-- /main-menu -->
 				</nav>
+				<div class="col-md-3 d-lg-none">
+					<div class="mobile-menu-toggle" style="position: fixed; top: 10px; right: 10px;">
+						<a href="#" class="open-mobile-menu" onclick="toggleMobileMenu()" style="font-size: 30px;"><i class="icon-menu"></i></a>
+					</div>
+					<div class="mobile-menu" id="mobileMenu" style="display:none;">
+						<ul>
+							<li><a href="index.php"><i class="icon-home"></i>Beranda</a></li>
+							<li class="mobile-dropdown">
+								<a href="#0" onclick="toggleDropdownMenu(event)"><i class="icon-doc-text"></i>Pages<i class="icon-down-open-mini"></i></a>
+								<ul class="mobile-dropdown-menu" style="display:none;">
+									<li><a href="artikel.php">Artikel</a></li>
+									<li><a href="penyakit.php">Penyakit</a></li>
+									<li><a href="obat.php">Obat</a></li>
+								</ul>
+							</li>
+							<li><a href="dokter.php"><i class="icon-user-md"></i>Dokter</a></li>
+							<li><a href="rs.php"><i class="icon-hospital"></i>Rumah Sakit</a></li>
+							<?php
+								if ($nama!='') {
+							?>
+							<li><a href="logout.php"><i class="icon-logout"></i>Logout</a></li>
+							<?php 
+								} 
+								else{
+							?>
+								<li><a href="register.php"><i class="icon-user-add"></i>Register</a></li>
+								<li><a href="login.php"><i class="icon-login"></i>Login</a></li>
+							<?php } ?>
+						</ul>
+					</div>
+				</div>
+				<script>
+					function toggleMobileMenu() {
+						var menu = document.getElementById('mobileMenu');
+						if (menu.style.display === 'none') {
+							menu.style.display = 'block';
+						} else {
+							menu.style.display = 'none';
+						}
+					}
+
+					function toggleDropdownMenu(event) {
+						event.preventDefault();
+						var dropdownMenu = event.target.nextElementSibling;
+						if (dropdownMenu.style.display === 'none') {
+							dropdownMenu.style.display = 'block';
+						} else {
+							dropdownMenu.style.display = 'none';
+						}
+					}
+				</script>
 			</div>
 		</div>
 		<!-- container -->
